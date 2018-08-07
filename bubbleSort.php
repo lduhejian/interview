d6052b86eda446be5be52af6f283f81e324af440
hejian <1217036457@qq.com>
2018-08-07 20:04:01 +0800

    bubbleSort

---
+++ bubbleSort.php
@@ -0,0 +1,20 @@
+<?php
+
+  function bubbleSort($arr) {
+    for ($i = 0; $i < count($arr); $i ++) {
+      for ($j = count($arr) - 1; $j > $i; $j --) {
+        if ($arr[$j] < $arr[$j - 1]) {
+          $temp = $arr[$j];
+          $arr[$j] = $arr[$j - 1];
+          $arr[$j - 1] = $temp;
+        }
+      }
+    }
+    return $arr;
+  }
+
+  $arr = [3, 2, 5, 1];
+  $sorted = bubbleSort($arr);
+  echo json_encode($sorted);
+
+ ?>
